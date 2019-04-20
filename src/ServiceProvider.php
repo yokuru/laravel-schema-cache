6f885be77e4282258a7978076be6ab8aa21fa056
yokuru <takashi.yokubo@gmail.com>
2019-04-20 10:30:07 +0900

    feat: register commands

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace Yokubo\SchemaCache;
 
 use Illuminate\Support\ServiceProvider as Provider;
+use Yokubo\SchemaCache\Commands\SchemaCache;
+use Yokubo\SchemaCache\Commands\SchemaClear;
 
 class ServiceProvider extends Provider
 {
@@ -16,6 +18,14 @@ class ServiceProvider extends Provider
 
     public function boot()
     {
+        echo "SchemaCacheServiceProvider";
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                SchemaCache::class,
+                SchemaClear::class,
+            ]);
+        }
+
         $schemaService = $this->app->make(SchemaService::class);
         $schemaHolder = $this->app->make(SchemaHolder::class);
         $schemaHolder->setTables($schemaService->describeTables());
